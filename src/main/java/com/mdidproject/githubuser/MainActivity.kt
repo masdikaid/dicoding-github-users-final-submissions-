61e5b155519c0d58823acb5c87ccc7325c497c06
masdika ID <masdikaid@gmail.com>
2022-06-11 11:46:11 +0700

    create main activity

---
+++ src/main/java/com/mdidproject/githubuser/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mdidproject.githubuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mdidproject.githubuser.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
