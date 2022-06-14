4639b2844235f95f2c4f2442bdb770aa4c82d116
masdika ID <masdikaid@gmail.com>
2022-06-14 15:53:15 +0700

    delete user list response

--- src/main/java/com/mdidproject/githubuser/response/UserConnectionResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.mdidproject.githubuser.response
-
-import com.google.gson.annotations.SerializedName
-
-data class UserConnectionResponse(
-	@field:SerializedName("UserConnectionResponse")
-	val users: List<UserItem>
-)
-
