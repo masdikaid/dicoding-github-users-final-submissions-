a163d00127123a543a47b023156be7f6b26f218a
masdika ID <masdikaid@gmail.com>
2022-06-14 15:52:26 +0700

    add menu

--- src/main/java/com/mdidproject/githubuser/MainActivity.kt
+++ src/main/java/com/mdidproject/githubuser/MainActivity.kt
@@ -2,6 +2,11 @@ package com.mdidproject.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.mdidproject.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.search_menu -> {
+                Log.i("Search-Button", "Clicked")
+                val action = FamousUserFragmentDirections.actionFamousUserFragmentToSearchUserFragment()
+                binding.fragmentContainerView.findNavController().navigate(action)
+                return true
+            }
+            else -> return true
+        }
     }
 }
