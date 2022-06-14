0df556a08bcfd46db7d962ba94ed8160695c060b
masdika ID <masdikaid@gmail.com>
2022-06-14 15:54:11 +0700

    add followers & following view model

--- src/main/java/com/mdidproject/githubuser/viewmodel/FollowingViewModel.kt
+++ src/main/java/com/mdidproject/githubuser/viewmodel/FollowingViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mdidproject.githubuser.api.GithubApi
-import com.mdidproject.githubuser.response.UserConnectionResponse
 import com.mdidproject.githubuser.response.UserItem
 import retrofit2.Call
 import retrofit2.Callback
