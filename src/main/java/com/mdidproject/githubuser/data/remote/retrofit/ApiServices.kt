dd4b6c34b5de7f55294801338ac5f8b183418810
masdika ID <masdikaid@gmail.com>
2022-06-26 16:09:29 +0700

    change app key to gradle properties

--- src/main/java/com/mdidproject/githubuser/data/remote/retrofit/ApiServices.kt
+++ src/main/java/com/mdidproject/githubuser/data/remote/retrofit/ApiServices.kt
@@ -1,32 +1,33 @@
-package com.mdidproject.githubuser.interfaces
+package com.mdidproject.githubuser.data.remote.retrofit
 
-import com.mdidproject.githubuser.response.UserItem
-import com.mdidproject.githubuser.response.UserListResponse
-import com.mdidproject.githubuser.response.UserResponse
+import com.mdidproject.githubuser.BuildConfig
+import com.mdidproject.githubuser.data.remote.response.UserItem
+import com.mdidproject.githubuser.data.remote.response.UserListResponse
+import com.mdidproject.githubuser.data.remote.response.UserResponse
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiServices {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     fun searchUsers(
         @Query("q") username: String
     ): Call<UserListResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     fun getUser(
         @Path("username") username: String
     ): Call<UserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     fun getUserFollowers(
         @Path("username") username: String
     ): Call<List<UserItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     fun getUserFollowing(
         @Path("username") username: String
     ): Call<List<UserItem>>
