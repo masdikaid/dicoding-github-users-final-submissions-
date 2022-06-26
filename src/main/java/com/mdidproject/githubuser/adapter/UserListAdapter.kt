f1d2e83005a4004d59638500b51752193264b5ad
masdika ID <masdikaid@gmail.com>
2022-06-26 16:28:55 +0700

    implementing fav button

--- src/main/java/com/mdidproject/githubuser/adapter/UserListAdapter.kt
+++ src/main/java/com/mdidproject/githubuser/adapter/UserListAdapter.kt
@@ -2,15 +2,19 @@ package com.mdidproject.githubuser.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.mdidproject.githubuser.R
+import com.mdidproject.githubuser.data.local.entity.UserEntity
+import com.mdidproject.githubuser.data.local.entity.UserWithFavStatusLiveData
 import com.mdidproject.githubuser.databinding.UserItemBinding
 import com.mdidproject.githubuser.interfaces.ItemAdapterCallback
-import com.mdidproject.githubuser.response.UserItem
+import com.mdidproject.githubuser.viewmodel.UserViewModel
 
-class UserListAdapter(private val listUser: List<UserItem>): RecyclerView.Adapter<UserListAdapter.ListViewHolder>() {
+class UserListAdapter(private val listUser: List<Any>, private val owner: LifecycleOwner, private val viewModel: UserViewModel): RecyclerView.Adapter<UserListAdapter.ListViewHolder>() {
 
-    private lateinit var itemCallback: ItemAdapterCallback<UserItem>
+    private lateinit var itemCallback: ItemAdapterCallback<UserWithFavStatusLiveData>
 
     class ListViewHolder(var binding: UserItemBinding): RecyclerView.ViewHolder(binding.root)
 
@@ -20,22 +24,83 @@ class UserListAdapter(private val listUser: List<UserItem>): RecyclerView.Adapte
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, pos: Int) {
-        val (avatar, username) = listUser[pos]
-        holder.binding.apply {
-            tvListUsername.text = username
 
-            Glide.with(holder.itemView.context)
-                .load(avatar)
-                .circleCrop()
-                .into(ivListAvatar)
+        when (val user = listUser[pos]) {
+            is UserWithFavStatusLiveData -> {
+                holder.binding.apply {
+                    tvListUsername.text = user.username
 
-            holder.itemView.setOnClickListener { view -> itemCallback.onItemClicked(view, listUser[holder.adapterPosition])}
+                    Glide.with(holder.itemView.context)
+                        .load(user.avatarUrl)
+                        .circleCrop()
+                        .into(ivListAvatar)
+                    user.isFavLiveData.observe(owner) {
+                        if (it) {
+                            btnFavorites.setImageResource(R.drawable.ic_baseline_favorite_fill_20)
+                        } else {
+                            btnFavorites.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+                        }
+
+                        btnFavorites.setOnClickListener { _ ->
+
+                            if (it) {
+                                viewModel.removeFav(user)
+                            } else {
+                                viewModel.setFav(user)
+                            }
+                        }
+
+                        holder.itemView.setOnClickListener { view ->
+                            itemCallback.onItemClicked(
+                                view,
+                                user
+                            )
+                        }
+                    }
+                }
+            }
+            is UserEntity -> {
+                holder.binding.apply {
+                    tvListUsername.text = user.username
+
+                    Glide.with(holder.itemView.context)
+                        .load(user.avatarUrl)
+                        .circleCrop()
+                        .into(ivListAvatar)
+
+                    val usersItem = UserWithFavStatusLiveData(
+                        user.username,
+                        user.avatarUrl,
+                        user.isFavorites,
+                        viewModel.isFav(user.username)
+                    )
+
+                    if (user.isFavorites) {
+                        btnFavorites.setImageResource(R.drawable.ic_baseline_favorite_fill_20)
+                    }else{
+                        btnFavorites.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+                    }
+
+                    btnFavorites.setOnClickListener {
+
+                        if (user.isFavorites){
+                            viewModel.removeFav(usersItem)
+                        }else{
+                            viewModel.setFav(usersItem)
+                        }
+                    }
+
+                    holder.itemView.setOnClickListener { view -> itemCallback.onItemClicked(view, usersItem)}
+                }
+            }
         }
+
     }
 
     override fun getItemCount(): Int = listUser.size
 
-    fun setAdapterItemCallback(callback: ItemAdapterCallback<UserItem>){
+    fun setAdapterItemCallback(callback: ItemAdapterCallback<UserWithFavStatusLiveData>){
         this.itemCallback = callback
     }
+
 }
